c9ad6eeb519f7fd79f55bd7e8c8854358326482d
Rukshan <arkruka@gmail.com>
2025-03-03 19:55:06 +0100

    feat: added post request

--- testly_api/requests.go
+++ testly_api/requests.go
@@ -1,13 +1,13 @@
 package testly
 
 import (
+	"bytes"
 	"net/http"
 )
 
 func GetRequest(url string, headers map[string]string, auth *HTTPStepAuth) (*http.Response, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
-
 	if err != nil {
 		return nil, err
 	}
@@ -23,5 +23,30 @@ func GetRequest(url string, headers map[string]string, auth *HTTPStepAuth) (*htt
 		req.Header.Set(key, value)
 	}
 
+	return client.Do(req)
+}
+
+func PostRequest(url string, headers map[string]string, auth *HTTPStepAuth, body *HTTPStepBody) (*http.Response, error) {
+	client := &http.Client{}
+	jsonBody, err := body.MarshalJSON()
+	if err != nil {
+		panic(err)
+	}
+
+	bodyReader := bytes.NewReader(jsonBody)
+
+	req, err := http.NewRequest("POST", url, bodyReader)
+	if err != nil {
+		return nil, err
+	}
+	if auth.Basic != nil {
+		req.SetBasicAuth(auth.Basic.Username, auth.Basic.Password)
+	}
+	if auth.Bearer != nil {
+		req.Header.Set("Authorization", "Bearer "+auth.Bearer.Token)
+	}
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
 	return client.Do(req)
 }
