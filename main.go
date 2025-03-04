7b4445438256cd684546af677bcd6209f57585a1
Rukshan <arkruka@gmail.com>
2025-03-04 09:02:49 +0100

    wip: work in progress main.go

--- main.go
+++ main.go
@@ -24,25 +24,50 @@ func main() {
 	json.Unmarshal(fileContent, &jsonTests)
 	steps := jsonTests.Steps
 	envs := jsonTests.Env
+
 	for _, step := range steps {
+		fmt.Println(*step.Name)
 		method := step.HTTP.Method
 		url := step.HTTP.URL
 		parsedUrl := testly.ReplacePlaceholders(url, envs)
+		queryParams := step.HTTP.Params
+
+		if queryParams != nil {
+			for key, value := range queryParams {
+				parsedUrl += fmt.Sprintf("%s=%s&", key, value)
+			}
+		}
 
 		switch method {
 		case "GET":
 			getRequest, err := testly.GetRequest(parsedUrl, step.HTTP.Headers, step.HTTP.Auth)
 			if err != nil {
+				panic(err)
+			}
+
+			requestBody := getRequest.Body
+			fmt.Println("GET", parsedUrl, requestBody)
+		case "POST":
+			postRequest, err := testly.PostRequest(parsedUrl, step.HTTP.Headers, step.HTTP.Auth, step.HTTP.Body)
+			if err != nil {
 				fmt.Println(err)
 				panic(err)
 			}
-			fmt.Println("GET", parsedUrl, getRequest)
-		case "POST":
-			fmt.Println("POST", parsedUrl)
+			fmt.Println("POST", parsedUrl, postRequest)
 		case "PUT":
-			fmt.Println("PUT", parsedUrl)
+			putRequest, err := testly.PutRequest(parsedUrl, step.HTTP.Headers, step.HTTP.Auth, step.HTTP.Body)
+			if err != nil {
+				fmt.Println(err)
+				panic(err)
+			}
+			fmt.Println("PUT", parsedUrl, putRequest)
 		case "DELETE":
-			fmt.Println("DELETE", parsedUrl)
+			deleteRequest, err := testly.DeleteRequest(parsedUrl, step.HTTP.Headers, step.HTTP.Auth)
+			if err != nil {
+				fmt.Println(err)
+				panic(err)
+			}
+			fmt.Println("DELETE", parsedUrl, deleteRequest)
 		default:
 			fmt.Println("Method not supported")
 		}
